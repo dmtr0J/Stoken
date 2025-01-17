ba541b5a24a0493f8395f01919e2c1a1543697a6
dmytro <badichel.dmytro@gmail.com>
2025-01-17 21:16:27 +0200

    Initialize Spring Boot API project

---
+++ backend/api/src/test/java/com/dmtr/stoken/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmtr.stoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
